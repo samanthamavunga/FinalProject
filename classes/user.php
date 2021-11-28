337e53a5cc73e23b02dbd17b1ef53e758c1183f9
Samantha Mavunga <samantha.mavunga@ashesi.edu.gh>
2021-11-28 04:14:03 +0000

    Updated Sunday

--- classes/user.php
+++ classes/user.php
@@ -10,54 +10,20 @@ user extend
 Class user extend database->
 
 <?php
-//MVC
-//Model that interacts with database
-//Code that interact with my database
 //connect to database class
 require_once (dirname(__FILE__)).'/../settings/db_class.php';
 
-class Post extends db_connection {
-    public function create($title, $body){
-        // sql query
-        $sql = "INSERT INTO `posts`(`title`, `body`) VALUES ('$title','$body')";//being called where there was an arror create
+class User extends db_connection{
+    // register user
+    public function register($username, $email, $password, $image_path){
+        $sql = "INSERT INTO `users`(`name`, `email`, `password`, `image`) VALUES ('$username', '$email', '$pasword', '$image_path')";
 
-        // run query
         return $this->db_query($sql);
     }
 
-    public function getPosts(){
-        //sql query
-        $sql = "SELECT * FROM `posts`";
-
-        //run query
-        return $this->db_query($sql);
-    }
-
-    public function getSinglePost($id){
-        // sql query
-        $sql = "SELECT * FROM `posts` WHERE `id`='$id'";
-
-        // run query
-        return $this->db_query($sql);
-    }
-
-    public function update($id, $title, $body){
-        // sql query
-        $sql = "UPDATE `posts` SET `title`='$title',`body`='$body' WHERE `id`='$id'";
-
-        // run query
-        return $this->db_query($sql);
-    }
-
-    public function delete($id){
-        // sql query
-        $sql = "DELETE FROM `posts` WHERE `id`='$id'";
-
-        // run query
+    // verify user email
+    public function verify_email($email){
+        $sql = "SELECT * FROM `users` WHERE `email`='$email'";
         return $this->db_query($sql);
     }
 }
-
-?>
-
-
