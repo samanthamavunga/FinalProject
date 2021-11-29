7f756b5a7c5f3043b412dbf173a07b35d374b82c
Samantha  Mavunga <samantha.mavunga@ashesi.edu.gh>
2021-11-29 16:40:24 +0000

    Update indent

--- classes/user_class.php
+++ classes/user_class.php
@@ -9,22 +9,3 @@ user extend
 
 Class user extend database->
 
-<?php
-//connect to database class
-require_once (dirname(__FILE__)).'/../settings/db_class.php';
-
-class User extends db_connection{
-    // register user
-    public function register($username, $password, $image_path){
-        $sql = "INSERT INTO `mentee`(`name`,`password`, `image`) VALUES ('$username', '$pasword', '$image_path')";
-
-        return $this->db_query($sql);
-    }
-
-
-public function verify_username($fname){
-    $sql = "SELECT * FROM `person` WHERE `fname`='$fname'";
-    return $this->db_query($sql);
-}
-
-}
